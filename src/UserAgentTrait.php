b3fca24177a511cdf997223189826ea9676c67fc
Pavlo Marenyuk <pavlomr@gmail.com>
2021-01-19 22:44:50 +0200

    php, curl in internals

--- src/UserAgentTrait.php
+++ src/UserAgentTrait.php
@@ -1,9 +1,32 @@
 <?php
+/*
+ * Copyright (c) 2021 Pavlo Marenyuk <pavlomr@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 namespace pavlomr\Service;
 
 use Exception;
 use ReflectionClass;
+
+use function curl_version;
 
 trait UserAgentTrait
 {
@@ -38,7 +61,12 @@ trait UserAgentTrait
                     $this->_callerVersionMinor(),
                     $this->_callerApplication(),
                     implode(' ', $parentsChain),
-                    $this->_callerExposeInternals() ? $internal : ''
+                    $this->_callerExposeInternals() ? sprintf(
+                        '%s PHP/%s curl/%s',
+                        $internal,
+                        PHP_VERSION,
+                        curl_version()['version']
+                    ) : ''
                 )
             );
         }
