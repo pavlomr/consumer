0d58aff948b99dd968545a9c1d0b8cf3392df1fb
Pavlo Marenyuk <pavlomr@gmail.com>
2022-11-22 14:10:07 +0200

    - CurlDecorator - CurlSFTP

---
+++ src/Consumer/CurlDecorator.php
@@ -0,0 +1,164 @@
+<?php
+/*
+ * Copyright (c) 2022 Pavlo Marenyuk <pavlomr@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace pavlomr\Service\Consumer;
+
+use pavlomr\Service\SingletonTrait;
+use pavlomr\Service\UserAgentTrait;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+
+use function curl_version;
+
+class CurlDecorator implements DecoratorInterface, LoggerAwareInterface
+{
+    protected const EXPOSE_INTERNALS = true;
+    use LoggerAwareTrait;
+    use SingletonTrait;
+    use UserAgentTrait;
+
+    protected string $base;
+    protected string $path;
+    private array    $auth = [];
+    /** @var false|resource */
+    private $client;
+
+    public function __construct(array $curlOptions = [])
+    {
+        $this->client = curl_init();
+        curl_setopt_array(
+            $this->getClient(),
+            $curlOptions + [
+                CURLOPT_HEADER         => false,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_ENCODING       => 'gzip',
+                CURLOPT_AUTOREFERER    => true,
+                CURLOPT_CONNECTTIMEOUT => 120,
+                CURLOPT_TIMEOUT        => 120,
+                CURLOPT_MAXREDIRS      => 5,
+                CURLOPT_SSL_VERIFYHOST => 2,
+                CURLOPT_SSL_VERIFYPEER => true,
+                CURLOPT_VERBOSE        => false,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_USERAGENT      => $this->userAgent(
+                    sprintf('PHP/%s curl/%s', PHP_VERSION, curl_version()['version'])
+                ),
+                CURLOPT_USERNAME       => $this->getAuth()['username'] ?? null,
+                CURLOPT_PASSWORD       => $this->getAuth()['password'] ?? null,
+            ]
+        );
+    }
+
+    public function __destruct()
+    {
+        curl_close($this->client);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAuth(): array
+    {
+        return $this->auth;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setAuth($auth): DecoratorInterface
+    {
+        $this->auth = $auth;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setPath(string $path): DecoratorInterface
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getBase(): string
+    {
+        return $this->base;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setBase(string $base): DecoratorInterface
+    {
+        $this->base = $base;
+
+        return $this;
+    }
+
+    protected function getClient()
+    {
+        return $this->client;
+    }
+
+    protected function mkUrl(...$arguments): string
+    {
+        return $this->getBase() .
+            implode(
+                '/',
+                array_map(
+                    fn($item) => rawurlencode($item),
+                    explode('/', $this->mkPath(...$arguments))
+                )
+            );
+    }
+
+    protected function mkPath(...$arguments): string
+    {
+        return $this->getPath() . implode('/', $arguments);
+    }
+
+    /**
+     * @param array $curlOptions
+     *
+     * @return bool|string
+     */
+    protected function _exec(array $curlOptions = [])
+    {
+        curl_setopt_array($this->getClient(), $curlOptions);
+
+        return curl_exec($this->getClient());
+    }
+}
---
+++ src/Consumer/CurlSFTP.php
@@ -0,0 +1,121 @@
+<?php
+/*
+ * Copyright (c) 2022 Pavlo Marenyuk <pavlomr@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace pavlomr\Service\Consumer;
+
+class CurlSFTP extends CurlDecorator
+{
+    public function __construct(array $curlOptions = [])
+    {
+        parent::__construct(
+            $curlOptions + [
+                CURLOPT_PROTOCOLS               => CURLPROTO_SFTP,
+                CURLOPT_FILETIME                => true,
+                CURLOPT_FTP_CREATE_MISSING_DIRS => CURLFTP_CREATE_DIR_RETRY,
+                CURLOPT_FTPLISTONLY             => true,
+                CURLOPT_SSH_PRIVATE_KEYFILE     => $this->getAuth()['keyfile'] ?? null,
+                CURLOPT_KEYPASSWD               => $this->getAuth()['keypasswd'] ?? null,
+            ]
+        );
+    }
+
+    public function getList(string $dir = '')
+    {
+        return $this->_exec(
+            [
+                CURLOPT_HTTPGET        => true,
+                CURLOPT_URL            => $this->mkUrl($dir),
+                CURLOPT_QUOTE          => [],
+                CURLOPT_INFILE         => null,
+                CURLOPT_FILE           => STDOUT,
+                CURLOPT_RETURNTRANSFER => true,
+            ]
+        );
+    }
+
+    public function getItem($name, $outputStream = null)
+    {
+        return $this->_exec(
+            [
+                CURLOPT_HTTPGET => true,
+                CURLOPT_URL     => $this->mkUrl($name),
+                CURLOPT_FILE    => $outputStream,
+                CURLOPT_INFILE  => null,
+                CURLOPT_QUOTE   => [],
+            ]
+        );
+    }
+
+    public function putItem($name, $inputStream)
+    {
+        return $this->_exec(
+            [
+                CURLOPT_UPLOAD => true,
+                CURLOPT_URL    => $this->mkUrl($name),
+                CURLOPT_INFILE => $inputStream,
+                CURLOPT_QUOTE  => [],
+            ]
+        );
+    }
+
+    public function mvItem($name1, $name2)
+    {
+        return $this->_exec(
+            [
+                CURLOPT_HTTPGET        => true,
+                CURLOPT_URL            => $this->mkUrl(),
+                CURLOPT_QUOTE          => [sprintf('rename %s %s', $this->mkPath($name1), $this->mkPath($name2))],
+                CURLOPT_INFILE         => null,
+                CURLOPT_FILE           => STDOUT,
+                CURLOPT_RETURNTRANSFER => true,
+            ]
+        );
+    }
+
+    public function rmItem($name)
+    {
+        return $this->_exec(
+            [
+                CURLOPT_HTTPGET        => true,
+                CURLOPT_URL            => $this->mkUrl(),
+                CURLOPT_QUOTE          => [sprintf('rm %s ', $this->mkPath($name))],
+                CURLOPT_INFILE         => null,
+                CURLOPT_FILE           => STDOUT,
+                CURLOPT_RETURNTRANSFER => true,
+            ]
+        );
+    }
+
+    public function getFiles(string $mask = null, string $dir = ''): iterable
+    {
+        foreach (explode(PHP_EOL, $this->getList($dir)) as $item) {
+            if (!empty($mask)) {
+                if (preg_match($mask, $item, $matches)) {
+                    yield $item;
+                }
+            } else {
+                yield $item;
+            }
+        }
+    }
+}
